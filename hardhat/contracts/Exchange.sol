ff520ab563fcc353dd4c0bf1e0608b3e9bfe3545
asanson <alexandresanson.pro@gmail.com>
2023-10-13 15:53:11 +0200

    AddLiquidity function without the LP Token minting

--- hardhat/contracts/Exchange.sol
+++ hardhat/contracts/Exchange.sol
@@ -8,7 +8,7 @@ contract Exchange is ERC20 {
     address public xelaTokenAddress;
 
     // Smart contract responsible of minting and issuing LP Tokens (is ERC20)
-    constructor(address _xelaTokenAddress) ERC20("ETH   XELA LP Token", "lpETHXELA") {
+    constructor(address _xelaTokenAddress) ERC20("ETH XELA LP Token", "lpETHXELA") {
         require(_xelaTokenAddress != address(0), "TOKEN_ADDRESS_PASSED_IS_NULL");
         xelaTokenAddress = _xelaTokenAddress;
     }
@@ -16,6 +16,26 @@ contract Exchange is ERC20 {
     // Returns the balance of Xela Token held by 'this' contract
     function getXelaReserve() public view returns (uint256) {
         return ERC20(xelaTokenAddress).balanceOf(address(this));
+    }
+
+    // Allow a user to add liquidity to the exchange
+    function addLiquidity(uint256 amountOfXela) external payable {
+
+        uint256 xelaReserve = getXelaReserve();
+
+        // If pools are empty, the first LP set the ratio of tokens
+        if (xelaReserve == 0) {
+            IERC20 xelaInst = ERC20(xelaTokenAddress);
+            xelaInst.transferFrom(xelaTokenAddress, address(this), amountOfXela);
+        }
+        else {
+            uint256 ethReserve = address(this).balance - msg.value;
+            uint256 rightXelaAmount = (msg.value * xelaReserve) / ethReserve;
+            require(amountOfXela >= rightXelaAmount, "INCORRECT_RATIO_OF_XELA_PROVIDED");
+            IERC20 xelaInst = ERC20(xelaTokenAddress);
+            xelaInst.transferFrom(xelaTokenAddress, address(this), rightXelaAmount);
+
+        }
     }
 
 
