6a0eced4bd01dd4820453376df149a25f9c5d93e
asanson <alexandresanson.pro@gmail.com>
2023-12-10 16:16:03 +0100

    fix: Correct temporarily the  xelaReserve.data BigInt conversion

--- frontend/components/pool/Pool.js
+++ frontend/components/pool/Pool.js
@@ -12,6 +12,9 @@ import { readContract, waitForTransaction, writeContract } from 'wagmi/actions'
 
 function Pool() {
 
+    // BigInt bug: variable to have the right Unit
+    const TO_ETH_UNIT = 10 ** 4;
+
     // State variable to show if liquidity is beeing added
     const [ isAdding, setIsAdding ] = useState(false);
 
@@ -66,12 +69,12 @@ function Pool() {
 
     // Function which returns the xla price
     const xlaPrice = () => {
-        return (formatEther(Number(ethReserve.data?.value)) / formatEther(BigInt(xelaReserve.data)))
+        return (formatEther(Number(ethReserve.data?.value)) / (formatEther(Number(xelaReserve.data)) / TO_ETH_UNIT))
     };
 
     // Function which returns the eth price
     const ethPrice = () => {
-        return (formatEther(BigInt(xelaReserve.data)) / formatEther(Number(ethReserve.data?.value)))
+        return ((formatEther(Number(xelaReserve.data)) / TO_ETH_UNIT) / formatEther(Number(ethReserve.data?.value)))
     };
 
     // Function to add liquidity to the ETH/XLA pool
@@ -121,7 +124,7 @@ function Pool() {
                         <div className={styles.pool}>
                             <p className={styles.poolToken}>XLA Pool</p>
                             <p className={styles.poolReserve}>
-                                {formatEther(BigInt(xelaReserve.data))} XLA
+                                {formatEther(Number(xelaReserve.data)) / TO_ETH_UNIT} XLA
                             </p>
                             <p className={styles.price}>1XLA = {xlaPrice()}ETH</p>
                         </div>
